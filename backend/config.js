68538511057a4546cd2ed55ed4786eb7a1378792
macmini62 <victorkangacha@gmail.com>
2024-03-08 04:23:34 +0300

    first commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = "mongodb+srv://mac62:likii2003@bookstore-application.bsnee8m.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore-Application";
